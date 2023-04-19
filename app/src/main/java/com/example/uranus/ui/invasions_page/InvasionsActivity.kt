5e50cf679cd222a3b332662f2af12ba2f2039cc0
GenryMacros <ulti1235@gmail.com>
2023-04-20 01:20:15 +0300

    InvasionsActivity warnings fix

--- app/src/main/java/com/example/uranus/ui/invasions_page/InvasionsActivity.kt
+++ app/src/main/java/com/example/uranus/ui/invasions_page/InvasionsActivity.kt
@@ -12,8 +12,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.Environment
 import android.os.IBinder
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -35,11 +33,11 @@ class InvasionsActivity : AppCompatActivity() {
     private val STORAGE_PERMISSION_CODE = 1
     private lateinit var invasionsViewModel: InvasionsViewModel
     private lateinit var binding: ActivityInvasionsBinding
-    private lateinit var authData: AuthenticationData;
-    private lateinit var videoView: VideoView;
-    private var camId: Int = 0;
-    private var mService: SocketService = SocketService();
-    private var downloadId: Int = 0;
+    private lateinit var authData: AuthenticationData
+    private lateinit var videoView: VideoView
+    private var camId: Int = 0
+    private var mService: SocketService = SocketService()
+    private var downloadId: Int = 0
     var mBound = false
 
     private val mConnection: ServiceConnection = object : ServiceConnection {
@@ -66,7 +64,7 @@ class InvasionsActivity : AppCompatActivity() {
             .setReadTimeout(30000)
             .setConnectTimeout(30000)
             .build()
-        PRDownloader.initialize(applicationContext, config);
+        PRDownloader.initialize(applicationContext, config)
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestStoragePermission()
         }
@@ -80,7 +78,7 @@ class InvasionsActivity : AppCompatActivity() {
         binding = ActivityInvasionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        videoView = findViewById(R.id.videoView);
+        videoView = findViewById(R.id.videoView)
         val table = binding.tableLayout
 
         invasionsViewModel = ViewModelProvider(this, InvasionsViewModelFactory())
@@ -95,71 +93,71 @@ class InvasionsActivity : AppCompatActivity() {
                 val bottomRowMargin = 0
                 val textSize = 16F
                 var id = 0
-                var textSpacer: TextView;
+                var textSpacer: TextView
 
-                while (table.getChildCount() > 1) {
-                    table.removeView(table.getChildAt(table.getChildCount() - 1))
+                while (table.childCount > 1) {
+                    table.removeView(table.getChildAt(table.childCount - 1))
                 }
 
                 invasionsData.invasions?.forEach{ invasion ->
                     run {
 
-                        textSpacer = TextView(this);
-                        textSpacer.setText("");
-                        val dateView = TextView(this);
+                        textSpacer = TextView(this)
+                        textSpacer.setText("")
+                        val dateView = TextView(this)
                         dateView.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
                                                                 TableRow.LayoutParams.WRAP_CONTENT)
                         dateView.gravity = Gravity.CENTER
-                        dateView.setPadding(5, 40, 0, 0);
+                        dateView.setPadding(5, 40, 0, 0)
 
                         val dateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
                         dateView.setTextColor(Color.parseColor("#df57d3"))
-                        dateView.text = dateFormat.format( (invasion.date ?: 0).toLong() * 1000L);
-                        dateView.textSize = 15F;
+                        dateView.text = dateFormat.format( (invasion.date ?: 0).toLong() * 1000L)
+                        dateView.textSize = 15F
 
                         val intrudersView = TextView(this)
                         intrudersView.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
                                                                  TableRow.LayoutParams.MATCH_PARENT)
-                        intrudersView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+                        intrudersView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
 
-                        intrudersView.gravity = Gravity.CENTER;
-                        intrudersView.setPadding(5, 15, 0, 0);
+                        intrudersView.gravity = Gravity.CENTER
+                        intrudersView.setPadding(5, 15, 0, 0)
 
-                        intrudersView.setTextColor(Color.parseColor("#000000"));
-                        intrudersView.text = "";
-                        intrudersView.textSize = 16F;
+                        intrudersView.setTextColor(Color.parseColor("#000000"))
+                        intrudersView.text = ""
+                        intrudersView.textSize = 16F
 
                         val videoButtonHolder = LinearLayout(this)
                         videoButtonHolder.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
                             150)
 
-                        videoButtonHolder.gravity = Gravity.CENTER;
-                        videoButtonHolder.setPadding(0, 15, 0, 30);
-                        val videoButton = Button(this);
+                        videoButtonHolder.gravity = Gravity.CENTER
+                        videoButtonHolder.setPadding(0, 15, 0, 30)
+                        val videoButton = Button(this)
                         videoButton.layoutParams = TableRow.LayoutParams(buttonWidth, buttonHeight)
-                        videoButton.setPadding(0, 0, 0, 0);
+                        videoButton.setPadding(0, 0, 0, 0)
 
-                        videoButton.setGravity(Gravity.CENTER);
-                        videoButton.setBackgroundResource(R.drawable.video_download_button);
+                        videoButton.gravity = Gravity.CENTER
+                        videoButton.setBackgroundResource(R.drawable.video_download_button)
                         videoButton.setOnClickListener {
                             downloadVideoByUrl(invasion.file_name, invasion.link)
                         }
                         videoButtonHolder.addView(videoButton)
 
-                        val tr = TableRow(this);
-                        tr.id = id;
-                        id += 1;
+                        val tr = TableRow(this)
+                        tr.id = id
+                        id += 1
                         val trParams = TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
-                            buttonHeight + tableRowOffset);
+                            buttonHeight + tableRowOffset)
                         trParams.setMargins(0, 0, 0,
-                            bottomRowMargin);
-                        tr.setPadding(0,20,0,0);
-                        tr.layoutParams = trParams;
+                            bottomRowMargin)
+                        tr.setPadding(0,20,0,0)
+                        tr.layoutParams = trParams
                         tr.setBackgroundColor(if (id % 2 == 0) Color.parseColor("#414244") else Color.parseColor("#37383a"))
-                        tr.addView(dateView);
-                        tr.addView(intrudersView);
-                        tr.addView(videoButtonHolder);
-                        table.addView(tr, trParams);
+                        tr.addView(dateView)
+                        tr.addView(intrudersView)
+                        tr.addView(videoButtonHolder)
+                        table.addView(tr, trParams)
                     }
                 }
             } else {
@@ -194,17 +192,18 @@ class InvasionsActivity : AppCompatActivity() {
                 override fun onDownloadComplete() {
                     showVideo(context, Environment.getExternalStorageDirectory().absolutePath + '/' + fileName)
                 }
-                override fun onError(error: com.downloader.Error?) {}
+                override fun onError(error: Error?) {}
             })
     }
 
+    @SuppressLint("InflateParams")
     private fun showVideo(context: Context, path: String?) {
         val inflater =
             ContextCompat.getSystemService(context, LayoutInflater::class.java)
 
-        val pw = PopupWindow(inflater?.inflate(com.example.uranus.R.layout.video_popup,
+        val pw = PopupWindow(inflater?.inflate(R.layout.video_popup,
             null, false), 1080, 1920, true)
-        pw.isFocusable = true;
+        pw.isFocusable = true
         val pwVideoView = pw.contentView.findViewById<VideoView>(R.id.videoView)
         val dismissButton1 = pw.contentView.findViewById<Button>(R.id.dismiss1)
         val dismissButton2 = pw.contentView.findViewById<Button>(R.id.dismiss2)
@@ -215,10 +214,10 @@ class InvasionsActivity : AppCompatActivity() {
             pw.dismiss()
         }
         val mediaController = MediaController(context)
-        mediaController.setAnchorView(pwVideoView);
+        mediaController.setAnchorView(pwVideoView)
         pwVideoView.setVideoPath(path)
         pwVideoView.setMediaController(mediaController)
-        pwVideoView.requestFocus();
+        pwVideoView.requestFocus()
         pwVideoView.start()
         pw.showAtLocation(pwVideoView, Gravity.CENTER, 0, -100)
     }
@@ -262,17 +261,3 @@ class InvasionsActivity : AppCompatActivity() {
     }
 }
 
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
-}
