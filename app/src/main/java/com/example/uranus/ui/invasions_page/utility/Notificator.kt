c747933f501806a00f98d9a4871bd19528404ddf
GenryMacros <ulti1235@gmail.com>
2023-04-24 18:56:19 +0300

    warnings fix

--- app/src/main/java/com/example/uranus/ui/invasions_page/utility/Notificator.kt
+++ app/src/main/java/com/example/uranus/ui/invasions_page/utility/Notificator.kt
@@ -3,14 +3,17 @@ package com.example.uranus.ui.invasions_page.utility
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.uranus.R
+import com.example.uranus.ui.broadcast.BroadcastActivity
 import com.example.uranus.ui.home_page.data.InvasionPayload
-import java.io.File
+
 
 class Notificator(
     val context: Context
@@ -34,6 +37,13 @@ class Notificator(
         if (!isInited) {
             return
         }
+        val intent = Intent(
+            context,
+            BroadcastActivity::class.java
+        )
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val notificationUri: Uri = Uri.parse("file:///android_asset/notification.mp3")
         with(NotificationManagerCompat.from(context)) {
             builder = NotificationCompat.Builder(context, channelId)
@@ -44,6 +54,7 @@ class Notificator(
                 .setSound(notificationUri)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
                 .setVibrate(longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400))
             notify(nextNotificationId, builder.build())
         }
